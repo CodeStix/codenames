804cc5247a8bbdd83053d44d1d35671103882302
Stijn Rogiest <reddusted@gmail.com>
2023-12-30 22:55:38 +0100

    Initial commit

---
+++ clean-pictures.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+const { execSync } = require("child_process");
+const path = require("path");
+const crypto = require("crypto");
+
+const inputDir = "/home/stijn/Pictures/Nieuwjaar 2023-2024/Stijn";
+const outputDir = "/media/stijn/Documents/Projects/codenames/public/images";
+
+const outputFiles = new Set();
+
+for (const filePath of fs.readdirSync(inputDir)) {
+    console.log(filePath);
+    const fullFilePath = path.join(inputDir, filePath);
+
+    const file = path.parse(fullFilePath);
+    const extLower = file.ext.toLowerCase();
+
+    const hash = execSync(`sha1sum "${fullFilePath}"`).toString("utf-8").split(" ")[0];
+    console.log(file.name, hash);
+
+    switch (extLower) {
+        case ".jpg":
+        case ".jpeg":
+        case ".png": {
+            outputFiles.add(hash + extLower);
+            fs.copyFileSync(fullFilePath, path.join(outputDir, hash) + extLower);
+            break;
+        }
+
+        case ".mp4":
+        case ".mov": {
+            // Convert to gif
+            try {
+                outputFiles.add(hash + ".gif");
+                execSync(`ffmpeg -i "${fullFilePath}" -qscale 0 "${path.join(outputDir, hash)}.gif"`);
+            } catch (ex) {
+                console.log(`Could not convert ${fullFilePath}`, ex);
+                process.exit(1);
+            }
+            break;
+        }
+
+        default: {
+            console.warn("Unknown extension", extLower);
+            process.exit(1);
+            break;
+        }
+    }
+}
+
+fs.writeFileSync(
+    path.join(outputDir, "files.json"),
+    JSON.stringify({
+        files: Array.from(outputFiles),
+    })
+);
+
+console.log(outputFiles.size, "files converted");
+
+// console.log("Output files", Array.from(outputFiles));
+
+// /home/stijn/Pictures/Nieuwjaar 2023-2024/Stijn/v09044g40000cc1ohjbc77u99jk48cj0.MP4
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function CodeNames(props: { possibleFiles: string[] }) {
+    const [files, setFiles] = useState(props.possibleFiles);
+
+    function shuffle() {
+        const f = [...files];
+        for (let i = 0; i < f.length; i++) {
+            let r = Math.floor(Math.random() * f.length);
+            let t = f[i];
+            f[i] = f[r];
+            f[r] = t;
+        }
+        console.log(f);
+        setFiles(f);
+    }
+
+    useEffect(() => {
+        shuffle();
+    }, []);
+
+    return (
+        <div style={{ display: "grid", gridTemplateColumns: "repeat(4, 200px)", gridTemplateRows: "repeat(4, 200px)", gap: "30px" }}>
+            {new Array(16).fill(0).map((_, i) => (
+                <div key={i} style={{ background: "#222", borderRadius: "1rem", overflow: "hidden", display: "relative" }}>
+                    <div
+                        style={{
+                            backgroundImage: `url("${"/images/" + files[i]}")`,
+                            backgroundPosition: "center center",
+                            backgroundRepeat: "no-repeat",
+                            backgroundSize: "cover",
+                            height: "100%",
+                        }}></div>
+                </div>
+            ))}
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [possibleFiles, setPossibleFiles] = useState<string[]>();
+
+    async function refreshPossbileFiles() {
+        const res = await fetch("/images/files.json");
+        return (await res.json()).files as string[];
+    }
+
+    useEffect(() => {
+        refreshPossbileFiles().then((e) => setPossibleFiles(e));
+    }, []);
+
+    return (
+        <div style={{ display: "flex", alignItems: "center", flexDirection: "column", justifyContent: "center", height: "100%" }}>
+            {/* <h2>codenames</h2> */}
+            {possibleFiles ? <CodeNames possibleFiles={possibleFiles} /> : <p>loading</p>}
+        </div>
+    );
 }
 
 export default App;
